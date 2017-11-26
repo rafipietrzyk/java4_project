07518d39ee81be6e8d2f02225f652ff866de2f00
Kamil Lorentowicz <lorentoka@wp.pl>
2017-11-26 12:42:49 +0100

    nowe zmiany

--- src/main/java/projekt/Main.java
+++ src/main/java/projekt/Main.java
@@ -12,18 +12,29 @@ public class Main {
         User user1 = new User("aaa", "bbb",Role.USER);
         User user2 = new User("ccc", "ddd",Role.USER);
         User user3 = new User("eee", "fff",Role.USER);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj login!");
-        String login = scanner.nextLine();
-        System.out.println("Podaj hasło!");
-        String pass = scanner.nextLine();
-        if(user1.getLogin().equals(login) && user1.getPass().equals(pass)){
-            System.out.println("Witaj, użytkowniku 1, jaką walutę chciałbyś kupić?");
+        for(int i = 0; i < 1;) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Podaj login!");
+            String login = scanner.nextLine();
+            System.out.println("Podaj hasło!");
+            String pass = scanner.nextLine();
+            if (user1.getLogin().equals(login) && user1.getPass().equals(pass)) {
+                System.out.println("Witaj, użytkowniku 1, jaką walutę chciałbyś kupić?");
+            // kupno walut!
 
-
-
-        }else{
-
+            } else if (user2.getLogin().equals(login) && user2.getPass().equals(pass)) {
+                System.out.println("Witaj, użytkowniku 2, jaką walutę chciałbyś kupić?");
+            // kupno walut
+            } else if (user3.getLogin().equals(login) && user3.getPass().equals(pass)) {
+                System.out.println("Witaj, użytkowniku 3, jaką walutę chciałbyś kupić?");
+            // kupno walut
+            } else if (admin.getLogin().equals(login) && admin.getPass().equals(pass)) {
+                System.out.printf("Witaj Administratorze %s ! Oto rodzaje walut i ich kursy!", Role.ADMIN);
+                // wszystkie kursy i waluty
+            } else {
+                System.out.println("Złe hasło lub login! Podaj jeszcze raz!");
+            }
+            i++;
         }
 
 
